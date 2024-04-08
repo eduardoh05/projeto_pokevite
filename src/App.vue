5e6c96111ca5c7a0390f4afbf76d7b70aa63e359
Eduardo Henrique Alves <ealves@PGE.MS>
2024-04-08 11:46:27 -0400

    implementação do boostrap e navbar

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref, reactive, onMounted, computed } from "vue";
+
+let num = ref(0);
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" href="#">
+        <img src="" alt="" width="30" height="24" />
+        Pokevite
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/home"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!--CONTENT-->
+  <div class="main">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped></style>
